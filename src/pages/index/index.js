d891250c4c878dc515bd51c57439d0e820c974cc
hu970804 <812510003@qq.com>
2018-01-31 11:37:00 +0800

    完成触底加载

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,18 +5,43 @@ import Vue from 'vue';
 import axios from 'axios';
 import url from 'js/api.js';
 
+import { InfiniteScroll } from 'mint-ui';
+Vue.use(InfiniteScroll);
 
 new Vue({
   el: '#app',
   data: {
-    lists: null
+    lists: null,
+    pageNumber: 1,
+    pageSize: 4,
+    loading: false, // 可以继续加载
+    allLoaded: false
   },
   created() {
+    this.getLists()
+  },
+  methods: {
+   getLists() {
+    if(this.allLoaded) return ;
+
+    this.loading = true;
     axios.post(url.hotLists, {
-      pageNumber: 1,
-      pageSize: 6
+      pageNumber: this.pageNumber,
+      pageSize: this.pageSize
     }).then(res => {
-      this.lists = res.data.list
+      let curLists = res.data.list;
+      // 判断所有数据加载完毕
+      if(curLists.length < this.pageSize){
+        this.allLoaded = true; 
+      }
+      if(this.lists) {
+        this.lists = this.lists.concat(curLists);
+      }  else {
+        this.lists = curLists ; //  第一次请求数据       
+      }
+      this.loading = false;
+      this.pageNumber++;
     })
+   }
   }
 })
