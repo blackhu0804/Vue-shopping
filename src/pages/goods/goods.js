eac117e3f341a3b04df9c956fc9627181409a197
hu970804 <812510003@qq.com>
2018-02-28 21:48:30 +0800

    完成详情页

--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -4,6 +4,7 @@ import './goods.css'
 import './goods_theme.css'
 import './goods_mars.css'
 import './goods_sku.css'
+import './goods_transition.css'
 
 import Vue from 'vue'
 import url from 'js/api.js'
@@ -19,11 +20,17 @@ let detailTab = ['商品详情','本店成交']
 new Vue({
   el: '#app',
   data: {
+    id,
     details: null,
     detailTab,
     tabIndex : 0,
     dealLists: null,
-    bannerLists: null
+    bannerLists: null,
+    skuType: 1,
+    showSku: false,
+    skuNum: 1,
+    isAddCart: false,
+    showAddMessage: false
   },
   created() {
     this.getDetails()
@@ -51,10 +58,42 @@ new Vue({
       axios.post(url.deal, {id}).then(res => {
         this.dealLists = res.data.data.lists
       })
+    },
+    chooseSku(type) {
+      this.skuType = type
+      this.showSku = true
+    },
+    changeSkuNum(num) {
+      if(num < 0 && this.skuNum === 1) return 
+      this.skuNum += num
+    },
+    addCart() {
+      axios.post(url.addCart, {
+        id,
+        number: this.skuNum
+      }).then(res => {
+        if(res.data.status === 200){
+          this.showSku = false
+          this.isAddCart = true
+          this.showAddMessage = true
+
+          setTimeout( () => {
+            this.showAddMessage = false
+          },1000)
+        }
+      })
     }
   },
   components: {
     Swipe
+  },
+  watch: {
+    showSku(val, old) {
+      document.body.style.overflow = val ? 'hidden' : 'auto'
+      document.querySelector('html').style.overflow = val ? 'hidden' : 'auto'
+      document.body.style.height = val ? '100' : 'auto'
+      document.querySelector('html').style.height = val ? '100%' : 'auto'
+    }
   },
   mixins: [mixin]
 })
