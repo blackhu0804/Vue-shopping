885395f346cdc614c61d9659b93aa86434e9f0e5
hu970804 <812510003@qq.com>
2018-03-04 23:48:27 +0800

    完成购物车的数据渲染

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -7,7 +7,11 @@ let url = {
   searchList :'/search/list',
   details :'/goods/details',
   deal :'/goods/deal',
-  addCart :'/cart/add'
+  addCart :'/cart/add',
+  cartLists :'/cart/list',
+  cartReduce : '/cart/reduce',
+  cartRemove :'/cart/remove',
+  cartMremove : '/cart/mremove' 
 }
 
 let host = 'http://rapapi.org/mockjsdata/31521'
--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -14,4 +14,33 @@ import './cart.css'
 //   }]
 // })
 
+import Vue from 'vue'
+import mixin from 'js/mixin.js'
+import axios from 'axios'
+import url from 'js/api.js'
 
+new Vue({
+  el: '.container',
+  data: {
+    lists: null
+  },
+  computer: {
+
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      axios.post(url.cartLists).then( res => {
+        this.lists = res.data.cartList
+        // this.lists.forEach(shop => {
+        //   shop.goodsList.forEach(good => {
+        //     good.checked = true
+        //   })
+        // })
+      })
+    }
+  },
+  mixins: [mixin]
+})
